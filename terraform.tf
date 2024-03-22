ecd88a6f4173b6a147097a911f96f9495c06134f
alexlofaso <79667867+alexlofaso@users.noreply.github.com>
2024-03-22 14:02:27 -0400

    fix backend

--- terraform.tf
+++ terraform.tf
@@ -5,9 +5,10 @@ terraform {
             version = "3.92.0"
         }
     }
-}
 
 backend "azurerm" {
     storage_account_name = "allofasotfstate"
     container_name = "askai"
+    key = "Fedairs/terraform.tfstate"
+    }
 }
