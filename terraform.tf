55098dddb86c07adfd2828316ea4908cc7467d50
alexlofaso <79667867+alexlofaso@users.noreply.github.com>
2024-03-22 12:40:00 -0400

    add terraform backend conifg

---
+++ terraform.tf
@@ -0,0 +1,13 @@
+terraform {
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = "3.92.0"
+        }
+    }
+}
+
+backend "azurerm" {
+    storage_account_name = "allofasotfstate"
+    container_name = "askai"
+}
