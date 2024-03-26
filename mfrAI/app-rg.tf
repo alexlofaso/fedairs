1aae6b06e94daff5289d06ec41652e555b4c60df
alexlofaso <79667867+alexlofaso@users.noreply.github.com>
2024-03-25 23:08:59 -0400

    fixed rg variable

--- mfrAI/app-rg.tf
+++ mfrAI/app-rg.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "apprg" {
-  name     = "${local.prefix}-${local.app_resource_group_name_resource_group_name}"
+  name     = "${local.prefix}-${local.app_resource_group_name}"
   location = local.location
 
   tags = local.tags
