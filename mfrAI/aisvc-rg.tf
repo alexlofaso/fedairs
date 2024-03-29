3cbe5d823283b3336eb779d4c71732a21261ac0e
alexlofaso <79667867+alexlofaso@users.noreply.github.com>
2024-03-29 14:22:33 -0400

    ai services

--- mfrAI/aisvc-rg.tf
+++ mfrAI/aisvc-rg.tf
@@ -19,3 +19,32 @@ module "cognitive_account_openai" {
   subnet_id             = "${azurerm_virtual_network.mfr-network.id}/subnets/${local.prefix}-aisvc-sn"
   tags                  = local.tags
 }
+
+module "cognitive_account_form_recognizer" {
+  source = "./_modules/CognitiveAccount"
+
+  location            = azurerm_resource_group.aisvcrg.location
+  resource_group_name = azurerm_resource_group.aisvcrg.name
+
+  custom_subdomain_name = "infoasst-fr-${local.rando}"
+  kind                  = "FormRecognizer"
+  name                  = "infoasst-fr-${local.rando}"
+  private_dns_zone_ids  = [azurerm_private_dns_zone.example.id]
+  sku_name              = "S0"
+  subnet_id             = "${azurerm_virtual_network.mfr-network.id}/subnets/${local.prefix}-aisvc-sn"
+  tags                  = local.tags
+}
+
+module "cognitive_account_enrichment" {
+  source = "./_modules/CognitiveAccount"
+
+  location            = azurerm_resource_group.aisvcrg.location
+  resource_group_name = azurerm_resource_group.aisvcrg.name
+
+  kind                 = "CognitiveServices"
+  name                 = "infoasst-enrichment-cog-${local.rando}"
+  private_dns_zone_ids = [azurerm_private_dns_zone.example.id]
+  sku_name             = "S0"
+  subnet_id            = "${azurerm_virtual_network.mfr-network.id}/subnets/${local.prefix}-aisvc-sn"
+  tags                 = local.tags
+}
